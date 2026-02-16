275fbc4d1ab841f4c363b2b3c679c5bf4f55274a
Luan Ferreira Campos <lfcampos.eab@gmail.com>
2026-02-16 11:43:06 -0300

    Feat: Adiciona Hero Section

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
-import './style.css'
+import './style.css' // Importa o CSS do Tailwind
 
-const app = document.querySelector('#app')
+// Importa as dependências
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // <--- ESSA LINHA É CRUCIAL. Sem ela, a animação quebra.
+// import { createIcons, icons } from 'lucide';
+import Alpine from 'alpinejs';
+
+// Inicializa Alpine
+window.Alpine = Alpine;
+Alpine.start();
+
+// Inicializa Ícones
+// createIcons({ icons });
+
+// Inicializa AOS (Animações)
+AOS.init({
+    duration: 800,
+    once: true,
+    offset: 0, // Mudei para 0 para garantir que apareça mesmo no topo
+});
